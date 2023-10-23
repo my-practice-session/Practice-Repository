a8166902a76b82944b38790403bf539b69d5d3c9
Sahil S <sahilmunna.shaik@gmail.com>
2023-10-23 19:24:03 +0530

    this is the second commit

---
+++ gitcommiting.sh
@@ -0,0 +1,5 @@
+all the files are commited for the 1st time in the local repository 
+and we are good to go and commit to the remote reposotory
+this is the final task
+and if this is it we can complete the git course in juat 3 days
+
